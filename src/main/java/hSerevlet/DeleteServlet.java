f9404a43c1bd778f3403456afe13e528a4480ca6
hitoki <h.nakamura.sys22@morijyobi.ac.jp>
2023-01-27 12:17:22 +0900

    Servlet daoの

--- src/main/java/hSerevlet/DeleteServlet.java
+++ src/main/java/hSerevlet/DeleteServlet.java
@@ -1,23 +1,27 @@
 package hSerevlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import deleteDAO.DeleteDAO;
+
 /**
  * Servlet implementation class servlet
  */
 @WebServlet("/servlet")
-public class servlet extends HttpServlet {
+public class DeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public servlet() {
+    public DeleteServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,8 +30,12 @@ public class servlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getParameter("UTF-8");
+		String id = request.getParameter("id");
+		DeleteDAO.deleteAccount(id);
+		String view ="WEB-INF/view/DeleteExecute.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+		dispatcher.forward(request, response);
 	}
 
 	/**
